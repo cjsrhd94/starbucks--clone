d34b33f7bffd790420700f1058922fce545a14e5
cjsrhd94 <cjsrhd94@gmail.com>
2022-05-11 22:45:54 +0900

    feat: Header 전역 배지 구현

--- js/main.js
+++ js/main.js
@@ -14,3 +14,23 @@ searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+// _.throttle(함수, 시간)
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        // 배지 숨기기
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+        // 배지 보이기
+    } else {
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
