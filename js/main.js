94d49d27d2d87c5c8c74bef2a0a6648962fcf29b
cjsrhd94 <cjsrhd94@gmail.com>
2022-05-16 03:36:01 +0900

    feat: promotion 부분 구현 및 swiper 추가

--- js/main.js
+++ js/main.js
@@ -56,3 +56,21 @@ new Swiper('.notice-line .swiper-container', {
     loop: true
 });
 
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination',
+        clickable: true
+    },
+
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
