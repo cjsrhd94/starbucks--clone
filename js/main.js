daf85ad8eb9b4d953aeec204c8018612f805b963
cjsrhd94 <cjsrhd94@gmail.com>
2022-05-18 07:37:27 +0900

    feat: awards section 구현

--- js/main.js
+++ js/main.js
@@ -75,6 +75,17 @@ new Swiper('.promotion .swiper-container', {
     }
 });
 
+new Swiper('.awards .swiper-container', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
