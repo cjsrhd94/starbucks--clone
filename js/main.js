05c29ce15d3fad96d3c7a12fe7bd1557361a1915
cjsrhd94 <cjsrhd94@gmail.com>
2022-05-18 07:18:59 +0900

    feat: section별 애니메이션 효과 추가

--- js/main.js
+++ js/main.js
@@ -107,3 +107,14 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+    new ScrollMagic
+        .Scene({
+            triggerElement: spyEl,   // 보여짐 여부를 감시할 요소를 지정
+            triggerHook: .8
+        })
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller()); 
+})
